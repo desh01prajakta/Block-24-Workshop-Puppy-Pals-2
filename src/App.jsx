45e5cfc0da5f2a012368fbb3df62068b662ec93c
desh01prajakta <prajudeshmukh11@gmail.com>
2023-12-21 22:52:15 -0600

    stage 2 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import './App.css';
 import {puppyList} from './data.js'
 
-
+function App(){
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)  
+  
+  return(
+    <div className="App">{ 
+    puppies.map((puppy) => {
+      return <p>{puppy.name}</p>
+    })
+ }</div>)
+}
+export default App 
 
 
